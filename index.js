1c68ad211326ef7ebc7591daaad16b4c129d2f56
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-07-30 17:16:49 +0530

    Server setup & deployment on vercel

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+// if (process.env.NODE_ENV !== "production") {
+app.use(cors());
+// }
+
+//static folder
+// app.use(express.static(__dirname + "/public"));
+
+const URL = "https://openlibrary.org/search.json?title=";
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    msg: "Everthing looks good",
+  });
+});
+
+app.get("/book/bookSearch", async (req, res) => {
+  const bookName = req.query.bookName;
+  console.log("in server");
+  const searchUrl = `${URL}${bookName}&limit=20`;
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  console.log(bookName);
+  try {
+    const response = await fetch(searchUrl, options);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).send({
+      msg: "Server error",
+    });
+  }
+});
+
+app.use(function (req, res, next) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
